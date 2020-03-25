8320eb41e3718309f466afea79a36aecf41712c1
chrisdugne <chris.dugne@uralys.com>
2020-03-26 00:00:05 +0100

    using nexus css

--- src/pages/map.js
+++ src/pages/map.js
@@ -2,6 +2,13 @@ import React, {useEffect, useState} from 'react';
 import Layout from '../components/layout';
 import styled from 'styled-components';
 
+const addExternalCss = src => {
+  const l = document.createElement('link');
+  l.setAttribute('rel', 'stylesheet');
+  l.setAttribute('href', src);
+  document.body.appendChild(l);
+};
+
 const addScript = (src, next) => {
   const s = document.createElement('script');
   s.setAttribute('src', src);
@@ -13,7 +20,8 @@ const addScript = (src, next) => {
 
 const _NexusWrapper = ({className}) => <div id="nexus" className={className} />;
 const NexusWrapper = styled(_NexusWrapper)`
-  background-color: #789;
+  background-color: #123;
+  color: '#eee';
   height: 100vh;
 `;
 
@@ -21,6 +29,7 @@ const MapPage = () => {
   useEffect(() => {
     const [, id] = document.URL.split('/map/');
 
+    addExternalCss('https://static.ptgem.com/apps/nexus-0.0.4.css');
     addScript('https://static.ptgem.com/apps/nexus-0.0.4.min.js', () => {
       window.Nexus.create({
         container: 'nexus',
