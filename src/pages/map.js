a0ea62cab275f32b4a9414bd1d990673cc7bd016
chrisdugne <chris.dugne@uralys.com>
2020-04-20 18:16:10 +0200

    updated to nexus 0.0.13

--- src/pages/map.js
+++ src/pages/map.js
@@ -29,8 +29,8 @@ const MapPage = () => {
   useEffect(() => {
     const [, id] = document.URL.split('/map/?id=');
 
-    addExternalCss('https://static.ptgem.com/apps/nexus-0.0.12.css');
-    addScript('https://static.ptgem.com/apps/nexus-0.0.12.min.js', () => {
+    addExternalCss('https://static.ptgem.com/apps/nexus-0.0.13.css');
+    addScript('https://static.ptgem.com/apps/nexus-0.0.13.min.js', () => {
       window.Nexus.create({
         container: 'nexus',
         config: `https://connections.ptgem.com/nimbus/${id}`
