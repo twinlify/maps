3ad0791329f756f069cf171bf12b93938f4f6b3f
Colin <46408738+colinernds@users.noreply.github.com>
2021-03-05 17:42:52 +0100

    Used the <Image /> tag
    Changed the <img> by <Image />

--- src/components/hero.js
+++ src/components/hero.js
@@ -11,7 +11,7 @@ const Hero = ({content}) => {
       <Image wrapperClassName={style.image} projectId="global" assetId="hero" />
       <div className={style.heroOverlay} />
       <div className={style.content}>
-        <img className={style.logo} src="https://static.twinlify.com/logos/logo.svg" alt="Twinlify Logo"/>
+        <Image wrapperClassName={style.logo} projectId="global" assetId="logo"/>
         <p className={style.punchLine}>{description}</p>
       </div>
     </div>
