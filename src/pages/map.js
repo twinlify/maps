7ed9ece5d91ea0cf283ab0fb787f865b44d53788
chrisdugne <chris.dugne@uralys.com>
2021-05-15 17:39:06 +0200

    updated nexus to 0.15.2

--- src/pages/map.js
+++ src/pages/map.js
@@ -24,7 +24,7 @@ const NexusWrapper = styled(_NexusWrapper)`
   height: 100vh;
 `;
 
-const NEXUS_VERSION = '0.15.1';
+const NEXUS_VERSION = '0.15.2';
 
 const MapPage = () => {
   useEffect(() => {
